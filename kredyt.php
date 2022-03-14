b1c4f27b833109d7a3f412f7b08d5885e758e55d
mssjanee <77584968+mssjanee@users.noreply.github.com>
2022-03-14 22:34:32 +0100

    Add files via upload

--- kredyt.php
+++ kredyt.php
@@ -1,44 +1,79 @@
 <?php
 require_once dirname(__FILE__).'/../config.php';
 
+// KONTROLER strony kalkulatora
 
-$kwota = $_REQUEST['kwota'];
-$ile_lat = $_REQUEST['ile_lat'];
-$oprocentowanie = $_REQUEST['oprocentowanie'];
+// W kontrolerze niczego nie wysyła się do klienta.
 
-if(!isset($kwota) && isset($ile_lat) && isset($oprocentowanie)) {
-	$messages [] = 'Błędnie wywołanie aplikacji.Brak jednego z parametrów.';
-	}
-	
-	if($kwota == "") {
-	$messages [] = 'Nie podano kwoty';
-	}
-    if($ile_lat == "") {
-	$messages [] = 'Nie podano ilości lat';
-    }
+include _ROOT_PATH.'/app/security/check.php';
 
-    if($oprocentowanie == "") {
-	$messages [] = 'Nie podano ilość procentów';
-    }
 
-if(empty($messages)){
-	if(!is_numeric($kwota)){
-	$messages [] = 'Kwota nie jest liczbą całkowitą';
-	}
-	if(!is_numeric($ile_lat)){
-		$messages [] = 'Ilość lat nie jest liczbą całkowitą';
-	}
-	if(!is_numeric($oprocentowanie)){
-		$messages [] = 'Oprocentowanie nie jest liczbą całkowitą';
-	}
+function getParams(&$kwota,&$ile_lat,&$proc){
+	$kwota = isset($_REQUEST['kwota']) ? $_REQUEST['kwota'] : null;
+	$ile_lat = isset($_REQUEST['ile_lat']) ? $_REQUEST['ile_lat'] : null;
+	$proc = isset($_REQUEST['proc']) ? $_REQUEST['proc'] : null;	
 }
 
-if(empty($messages)){
+
+function validate(&$kwota,&$ile_lat,&$proc,&$messages){
+
+	if ( ! (isset($kwota) && isset($ile_lat) && isset($proc))) {
+
+		return false;
+	}
+
+
+	if ( $kwota == "") {
+		$messages [] = 'Nie podano kwoty';
+	}
+	if ( $ile_lat == "") {
+		$messages [] = 'Nie podano ilości lat';
+	}
+	if ( $proc == "") {
+		$messages [] = 'Nie podano oprocentowania';
+	}
+
+
+	if (count ( $messages ) != 0) return false;
 	
+
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Kwota nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $ile_lat )) {
+		$messages [] = 'Ilość lat nie jest liczbą całkowitą';
+	}	
+	if (! is_numeric( $proc )) {
+		$messages [] = 'Oprocentowanie nie jest liczbą całkowitą';
+	}	
+
+	if (count ( $messages ) != 0) return false;
+	else return true;
+}
+
+function process(&$kwota,&$ile_lat,&$proc,&$messages,&$result){
+	
+	
+
 	$kwota = intval($kwota);
 	$ile_lat = intval($ile_lat);
-	$oprocentowanie = intval($oprocentowanie);
+	$proc = intval($proc);
 	
-	$result = ($oprocentowanie * $ile_lat * $kwota + $kwota)/($ile_lat * 12);
+	$result = ($proc * $ile_lat * $kwota + $kwota)/($ile_lat * 12);
 }
+
+$kwota = null;
+$ile_lat = null;
+$proc = null;
+$result = null;
+$messages = array();
+
+
+getParams($kwota,$ile_lat,$proc);
+if ( validate($kwota,$ile_lat,$proc,$messages) ) { 
+	process($kwota,$ile_lat,$proc,$messages,$result);
+}
+
+
 include 'kredyt_view.php';
--- kredyt_view.php
+++ kredyt_view.php
@@ -1,38 +1,57 @@
 <?php
-require_once dirname(__FILE__).'/../config.php'; ?>
+//Tu już nie ładujemy konfiguracji - sam widok nie będzie już punktem wejścia do aplikacji.
+//Wszystkie żądania idą do kontrolera, a kontroler wywołuje skrypt widoku.
+?>
 <!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
- <head>
-<meta charset="utf-8" />
-<title>Kredyty</title>
-</head> 
+<head>
+	<meta charset="utf-8" />
+	<title>Kalkulator</title>
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+</head>
 <body>
-<form action="<?php print(_APP_URL);?>/app/kredyt.php" method="post" />
-Kwota:
-<input type="text" name="kwota" value = "<?php isset($kwota)?print($kwota):print('');?>" /> <br /> <br /> 
-Ile lat:
-<input type="text" name="ile_lat" value="<?php isset($ile_lat)?print($ile_lat):print('')?>" /> <br /><br /> 
-Oprocentowanie:
-<input type="text" name="oprocentowanie" value="<?php isset($oprocentowanie)?print($oprocentowanie):print('')  ?>" /> <br /> <br /> 
-<input type="submit" value="Oblicz" />
-</form>
+
+<div style="width:90%; margin: 2em auto;">
+	<a href="<?php print(_APP_ROOT); ?>/app/inna_chroniona.php" class="pure-button">kolejna chroniona strona</a>
+	<a href="<?php print(_APP_ROOT); ?>/app/security/logout.php" class="pure-button pure-button-active">Wyloguj</a>
+</div>
+
+<div style="width:90%; margin: 2em auto;">
+
+<form action="<?php print(_APP_ROOT); ?>/app/kredyt.php" method="post" class="pure-form pure-form-stacked">
+	<legend>Kalkulator kredytów</legend>
+	<fieldset>
+		Kwota: 
+		<input  type="text" name="kwota" value="<?php out($kwota) ?>" />
+		Ilość lat:
+		<input  type="text" name="ile_lat" value="<?php out($ile_lat) ?>" />
+		Oprocentowanie:
+		<input  type="text" name="proc" value="<?php out($proc) ?>" />
+	
+	</fieldset>	
+	<input type="submit" value="Oblicz" class="pure-button pure-button-primary" />
+</form>	
 
 <?php
-if(isset($messages)){
-	if(count($messages) > 0){
-		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
-foreach ($messages as $key => $msg) {
-	echo '<li>'.$msg.'</li>';
-}
-echo '</ol>';
+
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin-top: 1em; padding: 1em 1em 1em 2em; border-radius: 0.5em; background-color: #f88; width:25em;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
 	}
 }
 ?>
 
 <?php if (isset($result)){ ?>
-<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<div style="margin-top: 1em; padding: 1em; border-radius: 0.5em; background-color: #ff0; width:25em;">
 <?php echo 'Rata misięczna: '.$result; ?>
 </div>
 <?php } ?>
+
+</div>
+
 </body>
 </html>
