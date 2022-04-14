00d1d7e50a2063c4ebd43e57942914d3a19826c6
mssjanee <77584968+mssjanee@users.noreply.github.com>
2022-04-14 16:07:05 +0200

    Add files via upload

---
+++ PAW3_07/app/controllers/CalcCtrl.class.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace app\controllers;
+
+//zamieniamy zatem 'require' na 'use' wskazując jedynie przestrzeń nazw, w której znajduje się klasa
+use app\forms\CalcForm;
+use app\transfer\CalcResult;
+
+/** Kontroler kalkulatora
+ * @author Przemysław Kudłacik
+ *
+ */
+class CalcCtrl {
+
+	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $result; //inne dane dla widoku
+
+	/** 
+	 * Konstruktor - inicjalizacja właściwości
+	 */
+	public function __construct(){
+		//stworzenie potrzebnych obiektów
+		$this->form = new CalcForm();
+		$this->result = new CalcResult();
+	}
+	
+	/** 
+	 * Pobranie parametrów
+	 */
+	public function getParams(){
+		$this->form->kwota = getFromRequest('kwota');
+		$this->form->ile_lat = getFromRequest('ile_lat');
+		$this->form->proc = getFromRequest('proc');
+	}
+	
+	/** 
+	 * Walidacja parametrów
+	 * @return true jeśli brak błedów, false w przeciwnym wypadku 
+	 */
+	public function validate() {
+		// sprawdzenie, czy parametry zostały przekazane
+		if (! (isset ( $this->form->kwota ) && isset ( $this->form->ile_lat ) && isset ( $this->form->proc ))) {
+			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+			return false;
+		}
+		
+		// sprawdzenie, czy potrzebne wartości zostały przekazane
+		if ($this->form->kwota == "") {
+			getMessages()->addError('Nie podano kwoty');
+		}
+		if ($this->form->ile_lat == "") {
+			getMessages()->addError('Nie podano liczby lat');
+		}
+		
+		if ($this->form->proc == "") {
+			getMessages()->addError('Nie podano oprocentowania');
+		}
+		
+		// nie ma sensu walidować dalej gdy brak parametrów
+		if (! getMessages()->isError()) {
+			
+			// sprawdzenie, czy $x i $y są liczbami całkowitymi
+			if (! is_numeric ( $this->form->kwota )) {
+				getMessages()->addError('Kwota nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->ile_lat )) {
+				getMessages()->addError('Liczba lat nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->proc )) {
+				getMessages()->addError('Oprocentowanie nie jest liczbą całkowitą');
+			}
+		}
+		
+		return ! getMessages()->isError();
+	}
+	
+	/** 
+	 * Pobranie wartości, walidacja, obliczenie i wyświetlenie
+	 */
+	public function action_calcCompute(){
+
+		$this->getParams();
+		
+		if ($this->validate()) {
+				
+			//konwersja parametrów na int
+			$this->form->kwota = intval($this->form->kwota);
+			$this->form->ile_lat = intval($this->form->ile_lat);
+			$this->form->proc = intval($this->form->proc);
+			getMessages()->addInfo('Parametry poprawne.');
+				
+			//wykonanie operacji
+		$this->result->result = ((($this->form->proc/100) * $this->form->ile_lat * $this->form->kwota) + $this->form->kwota)/($this->form->ile_lat * 12);	
+			getMessages()->addInfo('Wykonano obliczenia.');
+		}
+		
+		$this->generateView();
+	}
+	
+	public function action_calcShow(){
+		getMessages()->addInfo('Witaj w kalkulatorze');
+		$this->generateView();
+	}
+	
+	/**
+	 * Wygenerowanie widoku
+	 */
+	public function generateView(){
+global $user;
+		getSmarty()->assign('user',unserialize($_SESSION['user']));
+				
+		getSmarty()->assign('page_title','Super kalkulator - role');
+
+		getSmarty()->assign('form',$this->form);
+		getSmarty()->assign('res',$this->result);
+		
+		getSmarty()->display('CalcView.tpl');
+	}
+}
